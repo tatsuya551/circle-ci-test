6ea181fcf24b7185900585308a56aedb3c1396ab
tatsuya551 <seki0515ya@gmail.com>
2020-03-05 13:30:47 +0900

    circle_ci_setting

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   it 'nameが存在し,textが5文字以上存在する時保存される' do
-    item = Item.new(name: 'name', text: '123456')
+    item = Item.new(name: 'name', text: '1234')
     expect(item).to be_valid
   end
 
